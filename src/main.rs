aaf3a51c43f598e5f5fa38ce977539161544cbd4
decay <decayhonor@gmail.com>
2025-02-06 19:25:07 +0400

    support running both impls

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use std::env::args;
 use std::io;
 
-use mpl_interpreter::alt::repl;
+use mpl_interpreter::alt::repl as alt_repl;
+use mpl_interpreter::repl;
 
 fn main() {
-    // TODO: invoke alt with flag
-    println!("That's a Monkey Programming Language (MPL) interpreter.");
-    repl::start(io::stdin(), io::stdout());
+    let args = args();
+    match args.len() {
+        0 | 1 => {
+            println!("That's a Monkey Programming Language (MPL) interpreter.");
+            repl::start(io::stdin(), io::stdout());
+        }
+        2 => match args.last().unwrap().as_str() {
+            "--alt" => {
+                println!("Alternative Monkey Programming Language (MPL) interpreter");
+                println!("   ... don't hesitate");
+                alt_repl::start(io::stdin(), io::stdout());
+            }
+            other => panic!("only --alt is supported, got {other}"),
+        },
+        n => panic!("expected 1 argument at most"),
+    }
 }
